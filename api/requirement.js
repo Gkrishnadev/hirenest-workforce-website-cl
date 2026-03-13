2782da43140dfad3f42ecec1afb2e5d7ebbc7057
Gkrishna468 <114173433+Gkrishna468@users.noreply.github.com>
2026-03-13 09:18:41 +0530

    Update requirement.js

--- api/requirement.js
+++ api/requirement.js
@@ -1,1 +1,5 @@
+const { company, role, skills, location, job_type, contact_email } = req.body
 
+await supabase
+  .from('requirements')
+  .insert([{ company, role, skills, location, job_type, contact_email }])
