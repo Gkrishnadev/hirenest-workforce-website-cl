fe8d8348a3c36e69a8a406e93e10acb9afa5dab1
Gkrishna468 <114173433+Gkrishna468@users.noreply.github.com>
2026-03-13 09:19:20 +0530

    Update consultant.js

--- api/consultant.js
+++ api/consultant.js
@@ -1,1 +1,5 @@
+const { name, vendor_company, primary_skill, experience, location, contact_email } = req.body
 
+await supabase
+  .from('consultants')
+  .insert([{ name, vendor_company, primary_skill, experience, location, contact_email }])
