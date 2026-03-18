111cc85bbdb56f868603c5b54329f85208c4bed2
Gkrishna468 <114173433+Gkrishna468@users.noreply.github.com>
2026-03-19 00:32:24 +0530

    Update vendor.js

--- api/vendor.js
+++ api/vendor.js
@@ -2,11 +2,10 @@ export default async function handler(req, res) {
 
   const { name, email, company, phone, technologies, bench_size } = req.body
 
-  const { error } = await supabase
-    .from('vendors')
-    .insert([{ name, email, company, phone, technologies, bench_size }])
+  const { error } = await supabase.from('vendors').insert([{ name, email, company, phone, technologies, bench_size }
+  ])
 
   if (error) return res.status(500).json({ error })
-
+return
   res.status(200).json({ success: true })
 }
