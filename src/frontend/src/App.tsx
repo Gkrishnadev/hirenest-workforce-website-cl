39da6808bc8dd80bd5ad87b9432a55218199e32b
Gkrishna468 <114173433+Gkrishna468@users.noreply.github.com>
2026-03-18 20:41:47 +0530

    Update App.tsx

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -5,7 +5,7 @@ import { Outlet } from "@tanstack/react-router";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import About from "./pages/About";
-import Admin from "./pages/Admin";
+import Admin from "./pages/admin";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Partner from "./pages/Partner";
